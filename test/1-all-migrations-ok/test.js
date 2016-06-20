1e47b0d5f6cffacd48abe1e1856026d1d5ff0b99
Antti Pihlaja <antti.pihlaja@live.fi>
2016-06-20 17:58:05 +0300

    clean up database after tests

--- test/1-all-migrations-ok/test.js
+++ test/1-all-migrations-ok/test.js
@@ -4,6 +4,7 @@ var path = require('path');
 var chai = require('chai');
 var fs = require('fs');
 
+var MongoConn = require('../../src/utils/mongo-connection');
 var config = require('../config');
 var migration = new Migration(config);
 var dir = path.resolve(path.dirname(__filename), './migrations');
@@ -14,17 +15,38 @@ var filesPath = files.map(function(file) {
 });
 
 var should = chai.should();
+var expect = chai.expect;
 
 migration.add(filesPath);
 
 describe('Run migrations', function() {
+
+    var db;
+
+    before(function (done) {
+        new MongoConn(config).connect(function (err, _db) {
+            should.not.exist(err);
+            db = _db;
+            done();
+        });
+    })
+
+    after(function (done) {
+        db.dropDatabase(function (err, result) {
+            should.not.exist(err);
+            db.close(done);
+        });
+    });
+
     it('Must not have errors', function(done) {
         migration.migrate(function(err, result) {
             should.not.exist(err);
-
             result.should.be.an('array');
-            result.length.should.equal(filesPath.length);
+            result.should.have.lengthOf(1);
+            result.should.have.deep.property('[0].id', '1');
+            result.should.have.deep.property('[0].status', 'ok');
             done();
         });
     });
+
 });
